e182656d304e5493574b0fe37f4fc91b143e7426
ritiklakhwani <ritiklakhwani28@gmail.com>
2026-01-06 01:52:56 +0530

    realestate selling assignment done git status

---
+++ realEstate management/index.js
@@ -0,0 +1,25 @@
+import { propertyForSaleArr } from "/properties/propertyForSaleArr.js"
+import { placeholderPropertyObj } from "/properties/placeholderPropertyObj.js"
+
+function getPropertyHtml(properties = [placeholderPropertyObj]) {
+    return properties.map(property => {
+        const { propertyLocation, priceGBP, roomsM2, comment, image } = property
+
+        const totalSize = roomsM2.reduce((sum, room) => sum + room, 0)
+
+        return `
+        <section class="card">
+            <img src="/images/${image}">
+            <div class="card-right">
+                <h2>${propertyLocation}</h2>
+                <h3>£${priceGBP}</h3>
+                <p>${comment}</p>
+                <h3>${totalSize} m&sup2;</h3>
+            </div>
+        </section>
+        `
+    }).join('')
+}
+
+document.getElementById('container').innerHTML =
+    getPropertyHtml(propertyForSaleArr)
